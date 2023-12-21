dd75363705918df3f2bafbddc1b7170162fbe9b0
Ashruni <ashrunirkjuly21@gmail.com>
2023-12-21 15:33:38 +0530

    sixth commit ADDED ABSTRACT 23 QNS

--- abstractclasses.php
+++ abstractclasses.php
@@ -82,11 +82,7 @@ $obj_new1->message();
 
 ?>
 <!-- 22 -->
-<!-- 22.Create an abstract class 'Bank' with an abstract method 'getBalance'.
-$100, $150 and $200 are deposited in banks A, B and C respectively.
-'BankA', 'BankB' and 'BankC' are subclasses of class 'Bank', each having
-a method named 'getBalance'. Call this method by creating an object of
- each of the three classes -->
+
 <?php
  abstract class Bank{
     abstract public function getBalance();
@@ -116,6 +112,75 @@ $obj_new4=new BankB();
 $obj_new4->getBalance();
 $obj_new5=new BankC();
 $obj_new5->getBalance();
+
+
+ ?>
+ <!-- 23 -->
+ <!-- We have to calculate the percentage of marks obtained in 
+ three subjects (each out of 100) by student A and in four subjects
+  (each out of 100) by student B. Create an abstract class 'Marks' with 
+  an abstract method 'getPercentage'. It is inherited by two other classes 
+  'A' and 'B' each having a method with the same name which returns the percentage 
+  of the students. The constructor of student A takes the marks in three subjects
+   as its parameters and the marks in four subjects as its parameters for student B. 
+   Create an object for eac of the two 
+ classes and print the percentage of marks for both the students -->
+
+ <?php 
+ abstract class Mark{
+    public $mark1;
+    public $mark2;
+    public $mark3;
+    public function __construct($m1,$m2,$m3){
+        $this->mark1=$m1;
+        $this->mark2=$m2;
+        $this->mark3=$m3;
+
+
+    }
+    public function three_marks(){
+
+    }
+    abstract public function getPercentage();
+
+ }
+ class A extends Mark{
+    public $sum;
+    public $per;
+    public function getPercentage(){
+        $sum = $this->mark1 + $this->mark2 +$this->mark3 ;
+        $per= ($sum/300)*100;
+        echo "percentage in 3 subjects are "." $per "."\n";
+
+
+
+    }
+
+ }
+ class B extends Mark{
+    public $mark4;
+    public $add;
+    public $percentage;
+    public function __construct($m1,$m2,$m3,$m4){
+        $this->mark1=$m1;
+        $this->mark2=$m2;
+        $this->mark3=$m3;
+        $this->mark4=$m4;
+
+    }
+    public function getPercentage(){
+        $add = $this->mark1 + $this->mark2 +$this->mark3 + $this->mark4;
+        $percentage = ($add/400)*100;
+        echo "percentage of B in 4 subjects are ". $percentage ."\n";
+        // echo $add;
+    }
+
+
+ }
+ $new_obj =new A(96,98,90);
+ $new_obj->getPercentage();
+ $new_obj1=new B(88,89,99,97);
+ $new_obj1->getPercentage();
 
 
  ?>
